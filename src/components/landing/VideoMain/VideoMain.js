70db5ad05fc931accde3d7db09d22435988b224a
Anton Maryenkov <antonmaryenkov@gmail.com>
2022-11-12 15:22:05 +0700

    зациклил видео

--- src/components/landing/VideoMain/VideoMain.js
+++ src/components/landing/VideoMain/VideoMain.js
@@ -3,7 +3,7 @@ import videoMain from '../../../videos/video-main.mp4';
 function VideoMain() {
   return (
     <div className='video-main'>
-      <video className='video-main__video' autoPlay muted playsInline={true} disablePictureInPicture={true}>
+      <video className='video-main__video' autoPlay muted  loop playsInline={true} disablePictureInPicture={true}>
         <source src={videoMain} type='video/mp4'/>
       </video>
     </div>
